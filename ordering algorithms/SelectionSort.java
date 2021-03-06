9cfb8f9e9bcd53cda1cd72751956da9d8269ea3f
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2021-03-06 02:20:12 -0300

    Format selectionSort.java

--- ordering algorithms/SelectionSort.java
+++ ordering algorithms/SelectionSort.java
@@ -1,39 +1,29 @@
-
 public class SelectionSort {
-
 	public static void main(String[] args) {
-		
-        //varáveis
-        int[] array = {9,6,5,4,8,7,3};
 
-        for(int j = 0; j < array.length; j++){
-            int menor = j;
-            
-                for(int i = menor + 1; i < array.length; i++){
-                    if(array[i] < array[menor]){
-                        menor = i;
-                    }
-                }
-            
-                if (menor != j) {
-                    int t = array[j];
-                    array[j] = array[menor];
-                    array[menor] = t;
-                }
-      
-      
-    }
-        
-        
-        
-        
-        //imprime os valores
-        for(int i = 0; i < array.length; i++){
-            System.out.println(array[i]);
+    //varÃ¡veis
+    int[] array = {9,6,5,4,8,7,3};
+
+    for(int j = 0; j < array.length; j++){
+
+      int menor = j;
+
+      for(int i = menor + 1; i < array.length; i++){
+        if(array[i] < array[menor]){
+            menor = i;
         }
-		
-		
-		
-	}
+      }
 
+      if (menor != j) {
+          int t = array[j];
+          array[j] = array[menor];
+          array[menor] = t;
+      }
+    }
+    
+    //imprime os valores
+    for(int i = 0; i < array.length; i++){
+        System.out.println(array[i]);
+    }	
+	}
 }
